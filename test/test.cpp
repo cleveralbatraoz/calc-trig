dc2eec2d262f2e18a95f0275b561ae6ff2768ce1
Alexander Morozov <gelu.speculum@gmail.com>
2020-02-18 15:35:16 +0300

    Initial commit

---
+++ test/test.cpp
@@ -0,0 +1,104 @@
+#include "calc.h"
+
+#include <gtest/gtest.h>
+
+TEST(Calc, err)
+{
+    testing::internal::CaptureStderr();
+    EXPECT_DOUBLE_EQ(0, process_line(0, "fix"));
+    EXPECT_EQ("Unknown operation fix\n", testing::internal::GetCapturedStderr());
+    testing::internal::CaptureStderr();
+    EXPECT_DOUBLE_EQ(11, process_line(11, "sqrt"));
+    EXPECT_EQ("Unknown operation sqrt\n", testing::internal::GetCapturedStderr());
+    testing::internal::CaptureStderr();
+    EXPECT_DOUBLE_EQ(17, process_line(17, "\\ 11"));
+    EXPECT_EQ("Unknown operation \\ 11\n", testing::internal::GetCapturedStderr());
+}
+
+TEST(Calc, set)
+{
+    EXPECT_DOUBLE_EQ(0, process_line(0, "0"));
+    EXPECT_DOUBLE_EQ(0, process_line(0, "0000"));
+    EXPECT_DOUBLE_EQ(0, process_line(101, "0"));
+    EXPECT_DOUBLE_EQ(13, process_line(0, "13"));
+    EXPECT_DOUBLE_EQ(1234567890.0, process_line(1, "1234567890"));
+    testing::internal::CaptureStderr();
+    EXPECT_DOUBLE_EQ(1234567890.0, process_line(1, "12345678900000"));
+    EXPECT_EQ("Argument isn't fully parsed, suffix left: '0000'\n", testing::internal::GetCapturedStderr());
+}
+
+TEST(Calc, add)
+{
+    EXPECT_DOUBLE_EQ(7, process_line(0, "+7"));
+    EXPECT_DOUBLE_EQ(7, process_line(5, "+ 2"));
+    EXPECT_DOUBLE_EQ(7, process_line(5, "+ \t\t   2"));
+    testing::internal::CaptureStderr();
+    EXPECT_DOUBLE_EQ(1234567899.0, process_line(9, "+    12345678900000"));
+    EXPECT_EQ("Argument isn't fully parsed, suffix left: '0000'\n", testing::internal::GetCapturedStderr());
+}
+
+TEST(Calc, sub)
+{
+    EXPECT_DOUBLE_EQ(-11, process_line(0, "- 11"));
+    EXPECT_DOUBLE_EQ(0, process_line(0, "-0"));
+    EXPECT_DOUBLE_EQ(0, process_line(3, "-3"));
+    EXPECT_DOUBLE_EQ(-3, process_line(7, "-10"));
+}
+
+TEST(Calc, mul)
+{
+    EXPECT_DOUBLE_EQ(0, process_line(0, "* 0"));
+    EXPECT_DOUBLE_EQ(0, process_line(0, "*131"));
+    EXPECT_DOUBLE_EQ(0, process_line(99, "* 0"));
+    EXPECT_DOUBLE_EQ(8, process_line(2, "* 4"));
+    EXPECT_DOUBLE_EQ(-16, process_line(-4, "*4"));
+}
+
+TEST(Calc, div)
+{
+    EXPECT_DOUBLE_EQ(0, process_line(0, "/ 11"));
+    testing::internal::CaptureStderr();
+    EXPECT_DOUBLE_EQ(11, process_line(11, "/ 0"));
+    EXPECT_EQ("Bad right argument for division: 0\n", testing::internal::GetCapturedStderr());
+    EXPECT_DOUBLE_EQ(3, process_line(6, "/ 2"));
+    EXPECT_DOUBLE_EQ(0.7, process_line(7, "/ 10"));
+    EXPECT_DOUBLE_EQ(0.3333333333333333, process_line(1, "/ 3"));
+    EXPECT_DOUBLE_EQ(-0.5, process_line(-2, "/ 4"));
+}
+
+TEST(Calc, rem)
+{
+    EXPECT_DOUBLE_EQ(0, process_line(0, "/ 3"));
+    EXPECT_DOUBLE_EQ(0, process_line(4, "%4"));
+    EXPECT_DOUBLE_EQ(0, process_line(-24, "%4"));
+    EXPECT_DOUBLE_EQ(2, process_line(-13, "%5"));
+    testing::internal::CaptureStderr();
+    EXPECT_DOUBLE_EQ(-1, process_line(-1, "%0"));
+    EXPECT_EQ("Bad right argument for remainder: 0\n", testing::internal::GetCapturedStderr());
+}
+
+TEST(Calc, neg)
+{
+    EXPECT_DOUBLE_EQ(0, process_line(0, "_"));
+    EXPECT_DOUBLE_EQ(1, process_line(-1, "_"));
+}
+
+TEST(Calc, pow)
+{
+    EXPECT_DOUBLE_EQ(0, process_line(0, "^1"));
+    EXPECT_DOUBLE_EQ(0, process_line(0, "^2"));
+    EXPECT_DOUBLE_EQ(1, process_line(119, "^0"));
+    EXPECT_DOUBLE_EQ(37, process_line(37, "^1"));
+    EXPECT_DOUBLE_EQ(25, process_line(-5, "^2"));
+    EXPECT_DOUBLE_EQ(-27, process_line(-3, "^3"));
+}
+
+TEST(Calc, sqrt)
+{
+    EXPECT_DOUBLE_EQ(1, process_line(1, "SQRT"));
+    EXPECT_DOUBLE_EQ(0.7, process_line(0.49, "SQRT"));
+    EXPECT_DOUBLE_EQ(5, process_line(25, "SQRT"));
+    testing::internal::CaptureStderr();
+    EXPECT_DOUBLE_EQ(-1, process_line(-1, "SQRT"));
+    EXPECT_EQ("Bad argument for SQRT: -1\n", testing::internal::GetCapturedStderr());
+}
