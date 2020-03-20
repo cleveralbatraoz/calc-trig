052e318d47e62dcc551bcfeb4436b8ad45837a27
albatraozzz <dggrus@gmail.com>
2020-03-20 13:07:52 +0300

    code style fixes

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
   double current = 0;
   bool rad_on = false;
   for (std::string line; std::getline(std::cin, line);) {
-	current = process_line(current, rad_on, line);
-	std::cout << current << std::endl;
+    current = process_line(current, rad_on, line);
+    std::cout << current << std::endl;
   }
 }
